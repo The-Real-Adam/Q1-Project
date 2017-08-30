d4b17b0ea211db06001fe8bc366f84c765c10ccc
Adam <Adam@Adams-MacBook-Pro-3.local>
2017-08-30 14:00:48 -0600

    got working viable MVP

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,116 +1,153 @@
 $(document).ready(function() {
-  console.log("Document Ready");
+      console.log("Document Ready");
 
-  $("form").submit(function(event) {
-    event.preventDefault();
+      // GRAB VALUE OF THE BEER SELECT DROP DOWN
+      $("#beerSearch").click(function(event) {
+        beerStyleId = event.target.value
+        console.log("Beer Style Id is: ", beerStyleId)
+      })
 
-    // GRAB VALUE OF THE BEER SELECT DROP DOWN
-    var beerStyleId;
+      //the submission form that kicks it all off
+      $("form").submit(function(event) {
+        event.preventDefault();
 
-    // beer search term -- gets beer from the input feild
-    var beerSearch = $("#beerSearch").val().trim();
-    console.log("beerSearch is ", beerSearch)
-    getBeers(beerSearch)
+        // beer search term -- gets beer from the input feild
+        var beerSearch = $("#beerSearch").val().trim();
+        console.log("beerSearch is ", beerSearch)
+        getBeers(beerSearch)
 
-    // Album search term  -- gets artist from input feild
-    var albumSearch = $("#albumSearch").val().trim();
-    // console.log("albumSearch is ", albumSearch)
-    getAlbums(albumSearch)
-  });
+        // Album search term  -- gets artist from input feild
+        var albumSearch = $("#albumSearch").val().trim();
+        // console.log("albumSearch is ", albumSearch)
+        getAlbums(albumSearch)
+      });
 
-  //  Beer search function
-  function getBeers(beerSearch) {
-    console.log("getting beer info", beerSearch)
+      // add beer click event listener
+      $('#beerRandom').click(function(event) {
+        getBeers(beerSearch)
+      })
 
-    beerStyleId = 120;
-    var beerAPIKey = 'd396971583c5fd990eb18bf4f205d2dc';
+      // add album event listener
+      $('#albumRandom').click(function(event) {
+        getAlbums(albumSearch)
+      })
 
-    // the ajax call
-    // Example: "http://api.brewerydb.com/v2/beers?styleId=" + styleIdNum + "&key=d396971583c5fd990eb18bf4f205d2dc"
-    $.ajax({
-      url: `http://api.brewerydb.com/v2/beers?styleId=${beerStyleId}&key=${beerAPIKey}`,
-      method: 'GET',
-    }).done((responseBeer) => {
-      var beers = responseBeer.data;
-      console.log("My Beers Given a Style:", beers)
+      // add both event listener
+      $('#redoBoth').click(function(event) {
+        getBeers(beerSearch)
+        getAlbums(albumSearch)
+      })
 
-      // Randomly choose a beer in array beers
-      var displayedBeer = beers[Math.floor(Math.random() * beers.length)]
-      console.log("my random beer is", displayedBeer)
-      parseBeer(displayedBeer)
-    })
-  }
+      //  Beer search function
+      function getBeers(beerSearch) {
+        console.log("getting beer info", beerSearch)
 
-  function parseBeer(displayedBeer) {
-    let displayedBeerName = displayedBeer.name
-    let displayedBeerABV = displayedBeer.abv
-    let displayedBeerDesc= displayedBeer.description
-    let displayedBeerLabel = displayedBeer.labels.medium || `http://placekitten.com/g/300/300`
+        // beerStyleId = 120;
+        var beerAPIKey = 'd396971583c5fd990eb18bf4f205d2dc';
 
-    console.log("Beer Data to be rendered: ",displayedBeerName,displayedBeerABV,displayedBeerDesc, displayedBeerLabel)
-  }
+        // the ajax call
+        // Example: "http://api.brewerydb.com/v2/beers?styleId=" + styleIdNum + "&key=d396971583c5fd990eb18bf4f205d2dc"
+        $.ajax({
+          url: `http://api.brewerydb.com/v2/beers?styleId=${beerStyleId}&key=${beerAPIKey}`,
+          method: 'GET',
+        }).done((responseBeer) => {
+          var beers = responseBeer.data;
+          console.log("My Beers Given a Style:", beers)
 
+          // Randomly choose a beer in array beers
+          var displayedBeer = beers[Math.floor(Math.random() * beers.length)]
+          console.log("my random beer is", displayedBeer)
+          parseBeer(displayedBeer)
+        })
+      }
 
+      function parseBeer(displayedBeer) {
+        let displayedBeerName = displayedBeer.name
+        let displayedBeerABV = displayedBeer.abv
+        let displayedBeerDesc = displayedBeer.description
+        let displayedBeerLabel = displayedBeer.labels.medium || `http://placekitten.com/g/300/300`
 
-  // Album search function
-  function getAlbums(albumSearch) {
-    // console.log("getting album info", albumSearch);
+        console.log("Beer Data to be rendered: ", displayedBeerName, displayedBeerABV, displayedBeerDesc, displayedBeerLabel)
 
-    // the ajax call
-    // client ID 031b1f06c0c2483cb3939e84b632e3da
-    // client secret 64d8620a69614ae0aff51ce29f2a7d36
-    // the ACTUAL Authorization code: Bearer BQBcpHGBK4mAXga-rw_pyD453CQkb9FlCw6YUwX0_McGrLft2XgmsD8_KM4oSRdVboZP458sbxjLH8yxVMpYMS7WqtnQdpJfNZP7OpcVoeR86BTYcS8cpW18jQkWB0L9kzBm5e0S4A
-    $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        method: 'GET',
-        dataType: 'JSON',
-        headers: {
-          Authorization: "Bearer BQBf3CGEV22PPfFATuML2tu4Z6huQawFXG13m9h009ZqQauNkZtY6ukqMtA7emCtf9uLxvJYhnl3a3lH0xMm0w37GWLkgOUKDDffKWuzMDW1Th7VoLRed0RKzf-jeT2LmRdlajAgVQ"
-        },
-        data: {
-          q: `artist:${albumSearch}`,
-          type: "album",
-          limit: 18
+        renderBeer(displayedBeerName, displayedBeerABV, displayedBeerDesc, displayedBeerLabel)
+      }
+
+      // render your beer
+      function renderBeer(displayedBeerName, displayedBeerABV, displayedBeerDesc, displayedBeerLabel) {
+        let beerCard = $('div.MyBeer')
+        let beerCardInfo = $('<div>').append(`
+    <img class="card-img-top MyBeerImage" src="${displayedBeerLabel}" alt="Card image cap">
+    <div class="card-body MyBeer">
+      <h4 class="card-title">${displayedBeerName}</h4>
+      <p class="card-text">${displayedBeerDesc}</p>
+      <p class="card-text"><small class="text-muted">abv: ${displayedBeerABV}%</small></p>
+    </div>`)
+        beerCard.html(beerCardInfo)
+      }
+
+      // Album search function
+      function getAlbums(albumSearch) {
+        // console.log("getting album info", albumSearch);
+
+        // the ajax call
+        // client ID 031b1f06c0c2483cb3939e84b632e3da
+        // client secret 64d8620a69614ae0aff51ce29f2a7d36
+        // the ACTUAL Authorization code: Bearer BQBcpHGBK4mAXga-rw_pyD453CQkb9FlCw6YUwX0_McGrLft2XgmsD8_KM4oSRdVboZP458sbxjLH8yxVMpYMS7WqtnQdpJfNZP7OpcVoeR86BTYcS8cpW18jQkWB0L9kzBm5e0S4A
+        $.ajax({
+            url: 'https://api.spotify.com/v1/search',
+            method: 'GET',
+            dataType: 'JSON',
+            headers: {
+              Authorization: "Bearer BQBxRAMtNwlJ6429cR3vieVf17I-91qMUrMB4ASp__lhzkSbtT-m3Vo33T1yP5a27SGgg_XHRor7E_s4_xSrGaPTK4ozK-kmEMi9b4xsGocOlVNtbI9DEOmU2m4rO_xTqN06YzrzHA"
+            },
+            data: {
+              q: `artist:${albumSearch}`,
+              type: "album",
+              limit: 18
+            }
+          }).done((responseAlbum) => {
+            console.log("Spotify response: ", responseAlbum.albums.items)
+            var albums = responseAlbum.albums.items
+            // get a random album
+            var displayedAlbum = albums[Math.floor(Math.random() * albums.length)]
+            // console.log("my random album is", displayedAlbum)
+            parseAlbum(displayedAlbum)
+          })
+
+          .fail((err) => {
+            console.log("a bad thing happened with getting albums", err)
+          })
+
+      }
+
+      // extract albums from JSON AND add to DOM
+      function parseAlbum(displayedAlbum) {
+        console.log("parsing displayedAlbum:", displayedAlbum)
+
+        // go through albums, extract the url, name and image, then render
+        let displayedAlbumUrl = displayedAlbum.href
+        let displayedAlbumName = displayedAlbum.name
+        let displayedAlbumImageUrl = displayedAlbum.images ? displayedAlbum.images[0].url : ''
+
+        console.log("Album data to be rendered: ", displayedAlbumUrl, displayedAlbumName, displayedAlbumImageUrl);
+
+        renderAlbum(displayedAlbumUrl, displayedAlbumName, displayedAlbumImageUrl)
+
+      }
+
+      // add an album to the result UL
+      function renderAlbum(displayedAlbumUrl, displayedAlbumName, displayedAlbumImageUrl) {
+        let albumCard = $('div.MyAlbum')
+        let albumCardInfo = $('<div>').append(`
+           <img class="card-img-top MyAlbumImage" src="${displayedAlbumImageUrl}" alt="Card image cap">
+           <div class="card-body MyAlbum">
+             <h4 class="card-title">${albumSearch}</h4>
+             <p class="card-text">Album: ${displayedAlbumName}</p>
+             <p class="card-text"><small class="text-muted">Enjoy!</small></p>
+           </div>
+      `)
+
+          albumCard.html(albumCardInfo)
         }
-      }).done((responseAlbum) => {
-        console.log("Spotify response: ", responseAlbum.albums.items)
-        var albums = responseAlbum.albums.items
-        // get a random album
-        var displayedAlbum = albums[Math.floor(Math.random() * albums.length)]
-        // console.log("my random album is", displayedAlbum)
-        parseAlbum(displayedAlbum)
-      })
 
-      .fail((err) => {
-        console.log("a bad thing happened with getting albums", err)
-      })
-
-  }
-
-  // extract albums from JSON AND add to DOM
-  function parseAlbum(displayedAlbum) {
-    // console.log("parsing displayedAlbum:", displayedAlbum)
-
-    // go through albums, extract the url, name and image, then render
-    let displayedAlbumUrl = displayedAlbum.href
-    let displayedAlbumName = displayedAlbum.name
-    let displayedAlbumImageUrl = displayedAlbum.images ? displayedAlbum.images[0].url : ''
-
-    console.log("Album data to be rendered: ", displayedAlbumUrl, displayedAlbumName, displayedAlbumImageUrl);
-
-    renderAlbum(displayedAlbumUrl, displayedAlbumName, displayedAlbumImageUrl)
-
-  }
-
-  // add an album to the result UL
-  function renderAlbum(displayedAlbumUrl, displayedAlbumName, displayedAlbumImageUrl) {
-    let ul = $('ul.results')
-
-    let li = $('<li>').append(`<a href="${displayedAlbumUrl}">
-          <img src="${displayedAlbumImageUrl}">
-          <p class='caption'>${displayedAlbumName}</p>
-        </a>`)
-    ul.append(li)
-  }
-
-}); //end of document.ready
+      }); //end of document.ready
